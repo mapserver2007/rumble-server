05e8897ad75afbbe08a222c817f367209ea63d03
mapserver2007 <mapserver2007@gmail.com>
2016-10-09 21:28:35 +0900

    リクエストのデバッグ

--- models/norikae.rb
+++ models/norikae.rb
@@ -8,11 +8,11 @@ class Norikae
   end
 
   def search(message)
-
-    if /([^0-9a-zA-Z]+)→([^0-9a-zA-Z\s]+)(?:\s*)(\u59CB\u767A){0,}(\u7D42\u96FB){0,}/i =~ message
+    if /([^0-9a-zA-Z]+)→([^0-9a-zA-Z\s]+)(?:\s*)(\u59CB\u767A){0,}(\u7D42\u96FB){0,}/i =~ message.force_encoding("UTF-8")
       cway = 0
       trainType = "乗り換え"
-      p $3
+      puts "---"
+      puts message.force_encoding("UTF-8")
     end
 
   end
