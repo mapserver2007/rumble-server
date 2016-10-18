e546792db64e64d684c9e40bf9c47c5fdc8ebfd8
mapserver2007 <mapserver2007@gmail.com>
2016-10-18 23:55:39 +0900

    URL短縮試す

--- models/messaging.rb
+++ models/messaging.rb
@@ -26,7 +26,7 @@ class Messaging
       res = tumblr.get_image(keyword)
       if res[:state] = 200
         # @client.reply_message(token, {type: 'text', text: res[:image]})
-        hoge = "https://66.media.tumblr.com/0bd93e08e0df0ae1065a28f8cb36345c/tumblr_of7qc2NFX51qd227ho3_500.jpg"
+        hoge = "https://goo.gl/KpIbwJ"
         Logger.info res[:image]
         @client.reply_message(token, {type: 'image', originalContentUrl: hoge, previewImageUrl: hoge})
       else
