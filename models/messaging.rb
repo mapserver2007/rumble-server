7e17d43b3e3faed2172244f7a3a694ca7f31f414
mapserver2007 <mapserver2007@gmail.com>
2016-11-03 00:00:31 +0900

    クエリ修正

--- models/messaging.rb
+++ models/messaging.rb
@@ -50,8 +50,8 @@ class Messaging
         res[:contents].each do |content|
           columns << {thumbnailImageUrl: content[:img], text: content[:text], actions: [
             {type: 'uri', label: '大きい画像を見る', uri: content[:img]},
-            {type: 'postback', label: 'いいね！', data: "action=up&id=#{content[:id]}img=#{content[:img]}"},
-            {type: 'postback', label: 'ないわー', data: "action=down&id=#{content[:id]}img=#{content[:img]}",},
+            {type: 'postback', label: 'いいね！', data: "action=up&id=#{content[:id]}&img=#{content[:img]}"},
+            {type: 'postback', label: 'ないわー', data: "action=down&id=#{content[:id]}&img=#{content[:img]}"},
             {type: 'postback', label: 'これはひどい', data: 'action=replace&img=' + content[:img]},
           ]}
         end
